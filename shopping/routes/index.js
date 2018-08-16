64cd43a05ba8b5f4105e4076d54327871208d7a3
Bezsilny <julek200@gmail.com>
2018-08-16 20:17:28 +0200

    got rid of slicing into rows - it is done throught bootstrap

--- shopping/routes/index.js
+++ shopping/routes/index.js
@@ -5,12 +5,7 @@ var Product = require('../models/product.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Product.find(function (err, docs) {
-        var productChunks = [];//hehehehehhehehehehhehehehehhehehehehhehehehehhehehehehheheheheh
-        var chunkSize = 3;//hehehehehhehehehehhehehehehhehehehehhehehehehhehehehehheheheheh
-        for(var i=0; i<docs.length; i += chunkSize){
-            productChunks.push(docs.slice(i, i + chunkSize));
-        }
-        res.render('shop/index', { title: 'Shop here!', products: productChunks });
+        res.render('shop/index', { title: 'Shop here!', products: docs });
     });
 });
 
