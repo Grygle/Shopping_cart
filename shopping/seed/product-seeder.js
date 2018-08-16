961557ad97c9aeed12f317f5dd48fff789625afc
Bezsilny <julek200@gmail.com>
2018-08-16 16:52:50 +0200

    actual database - schema, sample records

---
+++ shopping/seed/product-seeder.js
@@ -0,0 +1,57 @@
+var Product = require('../models/product.js');
+
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/shopping", { useNewUrlParser: true });
+
+var products = [
+    new Product({
+    imagePath: 'https://placeimg.com/100/150/arch',
+    title: 'Hotel',
+    description: 'This is the best 4 star hotel in the city.',
+    price: 30
+    }),
+    new Product({
+        imagePath: 'https://placeimg.com/100/150/arch',
+        title: 'Hotel',
+        description: 'This is the best 4 star hotel in the city.',
+        price: 30
+    }),
+    new Product({
+        imagePath: 'https://placeimg.com/100/150/arch',
+        title: 'Hotel',
+        description: 'This is the best 4 star hotel in the city.',
+        price: 30
+    }),
+    new Product({
+        imagePath: 'https://placeimg.com/100/150/arch',
+        title: 'Hotel',
+        description: 'This is the best 4 star hotel in the city.',
+        price: 30
+    }),
+    new Product({
+        imagePath: 'https://placeimg.com/100/150/arch',
+        title: 'Hotel',
+        description: 'This is the best 4 star hotel in the city.',
+        price: 30
+    }),
+    new Product({
+        imagePath: 'https://placeimg.com/100/150/arch',
+        title: 'Hotel',
+        description: 'This is the best 4 star hotel in the city.',
+        price: 30
+    })
+];
+
+var done = 0;
+for (var i=0; i<products.length; i++){
+    products[i].save(function (err, result) {
+        done++;
+        if(done === products.length){
+            exit();
+        }
+    });
+}
+
+function exit() {
+    mongoose.disconnect();
+}
