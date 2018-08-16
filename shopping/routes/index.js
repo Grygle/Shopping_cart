06133fe1814881d68202c1602ee2eff16e6d2b4d
Bezsilny <julek200@gmail.com>
2018-08-16 19:11:25 +0200

    database usage

--- shopping/routes/index.js
+++ shopping/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping' });
+    Product.find(function (err, docs) {
+        var productChunks = [];//hehehehehhehehehehhehehehehhehehehehhehehehehhehehehehheheheheh
+        var chunkSize = 3;//hehehehehhehehehehhehehehehhehehehehhehehehehhehehehehheheheheh
+        for(var i=0; i<docs.length; i += chunkSize){
+            productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('shop/index', { title: 'Shop here!', products: productChunks });
+    });
 });
 
 module.exports = router;
--- shopping/seed/product-seeder.js
+++ shopping/seed/product-seeder.js
@@ -3,12 +3,27 @@ var Product = require('../models/product.js');
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/shopping", { useNewUrlParser: true });
 
+/*
+    =======================
+        DELETE DATABASE
+    =======================
+ */
+//     mongoose.connect('mongodb://localhost:27017/shopping', { useNewUrlParser: true }, function(){
+//     mongoose.connection.db.dropDatabase();
+//});
+
 var products = [
     new Product({
-    imagePath: 'https://placeimg.com/100/150/arch',
-    title: 'Hotel',
-    description: 'This is the best 4 star hotel in the city.',
-    price: 30
+        imagePath: 'https://placeimg.com/100/150/arch',
+        title: 'Hotel',
+        description: 'This is the best 4 star hotel in the city.',
+        price: 30
+    }),
+    new Product({
+        imagePath: 'https://placeimg.com/100/250/arch',
+        title: 'Hotel2',
+        description: 'This is the best 4 star hotel in the city.',
+        price: 30
     }),
     new Product({
         imagePath: 'https://placeimg.com/100/150/arch',
