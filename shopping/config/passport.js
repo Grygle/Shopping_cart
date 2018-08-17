feabce30bc0302c240713320f9efa9e7b95d1f01
Bezsilny <julek200@gmail.com>
2018-08-17 13:43:15 +0200

    sign-in works as intended

--- shopping/config/passport.js
+++ shopping/config/passport.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.serializeUser(function (sser, done) {
+passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
@@ -17,6 +17,7 @@ passport.use('local.signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, function (req, email, password, done) {
+
     //validation
     req.checkBody('email', 'Invalid email').notEmpty().isEmail();
     req.checkBody('password', 'Invalid password').notEmpty().isLength({min: 4});
@@ -28,6 +29,7 @@ passport.use('local.signup', new LocalStrategy({
         });
         return done(null, false, req.flash('error', messages));
     }
+
     User.findOne({'email': email}, function (err, user) {
        if(err){
            return done(err);
@@ -44,5 +46,36 @@ passport.use('local.signup', new LocalStrategy({
           }
           return done(null, newUser);
        });
+    });
+}));
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function(req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function(error) {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
+
+    //find user
+    User.findOne({'email': email}, function (err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, {message: 'No such a user found.'});
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, {message: 'Password is incorrect.'});
+        }
+        return done(null, user);
     });
 }));
