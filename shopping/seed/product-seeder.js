57be760b07a1ddbd710442111ab85ed8187bd409
Bezsilny <julek200@gmail.com>
2018-08-24 16:43:43 +0200

    changed images

--- shopping/seed/product-seeder.js
+++ shopping/seed/product-seeder.js
@@ -10,50 +10,50 @@ mongoose.connect("mongodb://localhost:27017/shopping", { useNewUrlParser: true }
  */
 //     mongoose.connect('mongodb://localhost:27017/shopping', { useNewUrlParser: true }, function(){
 //     mongoose.connection.db.dropDatabase();
-//});
+// });
 
 var products = [
     new Product({
-        imagePath: 'https://placeimg.com/100/150/arch',
-        title: 'Hotel',
-        description: 'This is the best 4 star hotel in the city.',
-        price: 30
+        imagePath: 'https://images.pexels.com/photos/573238/pexels-photo-573238.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        title: 'Umbrella with lights',
+        description: 'Do you want to shine in the night. This is a perfect umbrella for you.',
+        price: 20
     }),
     new Product({
-        imagePath: 'https://placeimg.com/100/250/arch',
-        title: 'Hotel2',
-        description: 'This is the best 4 star hotel in the city.',
-        price: 30
+        imagePath: 'https://images.pexels.com/photos/450060/pexels-photo-450060.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        title: 'Chinese umbrella',
+        description: 'Umbrella with orient style.',
+        price: 8
     }),
     new Product({
-        imagePath: 'https://placeimg.com/100/150/arch',
-        title: 'Hotel',
-        description: 'This is the best 4 star hotel in the city.',
-        price: 30
+        imagePath: 'https://images.pexels.com/photos/117/city-sunny-people-sun.jpg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        title: 'Green umbrella',
+        description: 'Umbrella in green colour.',
+        price: 13
     }),
     new Product({
-        imagePath: 'https://placeimg.com/100/150/arch',
-        title: 'Hotel',
-        description: 'This is the best 4 star hotel in the city.',
-        price: 30
+        imagePath: 'https://images.pexels.com/photos/533974/pexels-photo-533974.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        title: 'Natural umbrella',
+        description: 'This is the best if you are vegan, gluten free hipsta.',
+        price: 40
     }),
     new Product({
-        imagePath: 'https://placeimg.com/100/150/arch',
-        title: 'Hotel',
-        description: 'This is the best 4 star hotel in the city.',
-        price: 30
+        imagePath: 'https://images.pexels.com/photos/459882/pexels-photo-459882.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        title: 'Vintage umbrella',
+        description: 'Stylish vintage umbrella.',
+        price: 15
     }),
     new Product({
-        imagePath: 'https://placeimg.com/100/150/arch',
-        title: 'Hotel',
-        description: 'This is the best 4 star hotel in the city.',
-        price: 30
+        imagePath: 'https://images.pexels.com/photos/1207926/pexels-photo-1207926.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        title: 'Rainy umbrella',
+        description: 'This umbrella protects you from the rain for sure.',
+        price: 21
     }),
     new Product({
-        imagePath: 'https://placeimg.com/100/150/arch',
-        title: 'Hotel',
-        description: 'This is the best 4 star hotel in the city.',
-        price: 30
+        imagePath: 'https://images.pexels.com/photos/940034/pexels-photo-940034.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        title: 'Clear umbrella',
+        description: 'Want to show off with our outfit? Pick this one so as everyone can see your new coat.',
+        price: 11
     })
 ];
 
